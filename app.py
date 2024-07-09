0770013ec830d0688589bd8eb4027333711c8518
danishred <83757129+danishred@users.noreply.github.com>
2024-07-09 16:59:00 +0530

    Trying TLS = True

--- app.py
+++ app.py
@@ -15,7 +15,13 @@ os.environ.get('KV_URL')
 
 app.config["SESSION_PERMANENT"] = True  # Make the session permanent
 app.config["SESSION_TYPE"] = "redis"  # Use Redis for session storage
-app.config["SESSION_REDIS"] = Redis.from_url(os.environ.get('KV_URL')) # Configure redis connection
+
+redis_url = os.environ.get('KV_URL')
+
+if redis_url.startswith('redis://'):
+    redis_url = 'rediss://' + redis_url[len('redis://'):]
+
+app.config["SESSION_REDIS"] = Redis.from_url(redis_url)
 
 # Initialize the session
 Session(app)
